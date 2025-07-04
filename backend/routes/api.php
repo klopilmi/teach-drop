8a062b996748af2491ceab5f4614863efb3dba67
clofel <campuganmaec@gmail.com>
2025-07-04 18:26:45 +0800

    chore: vendor sail, migrations, and category crud

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{category}', [CategoryController::class, 'update']);
+Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+
+
+
+
